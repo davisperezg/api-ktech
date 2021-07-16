2f9a6a1cc2ffcd168d505a2f969f5509a92c664e
Davis Keiner Pérez Guzmán <davisperezg@gmail.com>
2021-07-16 01:34:02 -0500

    fix orden de validaciones en modelo

--- src/model/dto/inputs/create-model.input.ts
+++ src/model/dto/inputs/create-model.input.ts
@@ -5,17 +5,17 @@ import { IsNotEmpty, Length, Matches } from 'class-validator';
 @InputType()
 export class CreateModelInput {
   @Field()
+  @Matches(/^[A-Za-záéíóúÑñ\s]+$/, {
+    message: 'El nombre solo puede contener letras',
+  })
+  @IsNotEmpty({ message: 'Debe completar la marca' })
+  brand: string;
+
+  @Field()
   @Matches(/^[A-Za-z0-9áéíóúÑñ\s-]+$/, {
     message: 'El nombre solo puede contener letras, numeros y guiones',
   })
   @Length(3, 55, { message: 'El nombre debe ser mayor a 2 caracteres' })
   @IsNotEmpty({ message: 'Debe completar el nombre' })
   name: string;
-
-  @Field()
-  @Matches(/^[A-Za-záéíóúÑñ\s]+$/, {
-    message: 'El nombre solo puede contener letras',
-  })
-  @IsNotEmpty({ message: 'Debe completar la marca' })
-  brand: string;
 }
